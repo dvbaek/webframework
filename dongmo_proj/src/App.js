1c44f11cc5c18eb53e948cc540734b8f91d12fd3
DaBeenBaek <bdb7979@naver.com>
2023-11-16 19:10:48 +0900

    practice

---
+++ dongmo_proj/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import MainPage from "./pages/MainPage";
+import IntroPage from "./pages/IntroPage";
+import JoinPage from "./pages/JoinPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage/>}></Route>
+          <Route path="/intro" element={<IntroPage/>}></Route>
+          <Route path="/join" element={<JoinPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ dongmo_proj/src/components/IntroBox.js
@@ -0,0 +1,31 @@
+// IntroBox.js
+import React from 'react';
+import './IntroBox.css';
+import insta_logo from '../images/insta_logo.png';
+import blog_logo from '../images/blog_logo.png';
+
+function IntroBox({ crewData }) {
+    const { crewName, crewImg, simple_intro, insta, blog } = crewData;
+
+    return (
+        <div className="intro-crew-container">
+            <div className="img-slide-container">
+                <img className="intro-crewImg" src={crewImg} alt={crewName} />
+                <div className="overlay">
+                    <div className="text">
+                        <p>{simple_intro}</p>
+                        <a href={insta} target="_blank" rel="noopener noreferrer"> 
+                            <img className="instagram" src={insta_logo} alt="Instagram Logo" />
+                        </a>
+                        <a href={blog} target="_blank" rel="noopener noreferrer"> 
+                            <img className="blog" src={blog_logo} alt="Blog Logo" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="intro-crewName">{crewName}</div>
+        </div>
+    );
+}
+
+export default IntroBox;
